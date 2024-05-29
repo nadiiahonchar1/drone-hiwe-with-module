7d91f2409f57f45e691b7a661cf0348a9212a1a1
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-05-29 15:51:50 +0300

    added drone animation

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,9 +1,43 @@
+'use client';
+import { useEffect, useRef } from 'react';
 import Link from '../Link';
 import headerNavLinks from '../../data/headerNavLinks';
 import Image from 'next/image';
 import Logo from '@/assets/Logo.png';
+import dron from '@/assets/drone_6316693.png';
 import style from './footer.module.css';
+
 const Footer = () => {
+  const droneRef = useRef<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    const drone = droneRef.current;
+
+    const handleMouseEnter = () => {
+      if (drone && !drone.classList.contains(style.animate)) {
+        drone.classList.add(style.animate);
+      }
+    };
+
+    const handleAnimationEnd = () => {
+      if (drone) {
+        drone.classList.remove(style.animate);
+      }
+    };
+
+    if (drone) {
+      drone.addEventListener('mouseenter', handleMouseEnter);
+      drone.addEventListener('animationend', handleAnimationEnd);
+    }
+
+    return () => {
+      if (drone) {
+        drone.removeEventListener('mouseenter', handleMouseEnter);
+        drone.removeEventListener('animationend', handleAnimationEnd);
+      }
+    };
+  }, [droneRef]);
+
   return (
     <footer className={style.footer}>
       <div className={style.container}>
@@ -23,6 +57,17 @@ const Footer = () => {
               <span className={style.spanLink}></span>
             </Link>
           ))}
+        </div>
+        <div className={style.dronBox}>
+          <Image
+            src={dron}
+            alt="Ілюстративне фото дрона"
+            priority={true}
+            width={40}
+            height={40}
+            className={style.drone}
+            ref={droneRef}
+          />
         </div>
       </div>
     </footer>
