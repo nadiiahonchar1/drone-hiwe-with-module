983f4474b7ab29fb88d023737d531a91e3fdb36c
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-05-31 17:10:56 +0300

    add test

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,9 @@
+'use client';
+import { useEffect } from 'react';
 import LoginForm from '../admin/components/LoginForm/LoginForm';
 import Wrapper from '../components/Wrapper/Wrapper';
 import LogOutButton from './components/LogOutButton/LogOutButton';
+import { getToken, isLoggedIn } from './api/auth';
 export default function Admin() {
   return (
     <>
