3cc8ca74f4b85fb06a2eaa79ce7af1be7af46363
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-05-31 17:01:21 +0300

    remove getAnalytics

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,13 @@
 import LoginForm from '../admin/components/LoginForm/LoginForm';
+import Wrapper from '../components/Wrapper/Wrapper';
 import LogOutButton from './components/LogOutButton/LogOutButton';
 export default function Admin() {
   return (
     <>
-      <LoginForm />
-      <LogOutButton />
+      <Wrapper>
+        <LoginForm />
+        <LogOutButton />
+      </Wrapper>
     </>
   );
 }
