313875d5ae6aa0b6ccf833405a7a34b60039a4b5
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-06-05 12:55:29 +0300

    fix contact form

--- src/app/components/ContactForm/ContactForm.tsx
+++ src/app/components/ContactForm/ContactForm.tsx
@@ -1,16 +1,23 @@
 'use client';
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import style from './contactForm.module.css';
 
-const ContactForm = () => {
+type FormValues = {
+  name: string;
+  phone: string;
+  email: string;
+  message: string;
+};
+
+const ContactForm: React.FC = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<FormValues>();
 
-  const onSubmit = async (data: any) => {
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
     try {
       // await sendFormDataToDB(data);
       alert('Форма успішно відправлена!');
@@ -28,7 +35,6 @@ const ContactForm = () => {
         </label>
         <input
           id="name"
-          name="name"
           className={style.input}
           placeholder="Ім'я"
           {...register('name')}
@@ -41,15 +47,14 @@ const ContactForm = () => {
         </label>
         <input
           id="phone"
-          name="phone"
           className={style.input}
           placeholder="+380XX XXX XXXX*"
           data-empty-notice="Ми не зможемо з вами оперативно зв'язатись без номеру телефону"
-          {...register('phone', { required: true })}
+          {...register('phone', { required: "Номер телефону є обов'язковим" })}
         />
         {errors.phone && (
           <span className={style.error}>
-            {errors.phone.message || errors.phone.ref.dataset.emptyNotice}
+            {errors.phone.message || errors.phone.ref?.dataset?.emptyNotice}
           </span>
         )}
       </div>
@@ -60,15 +65,16 @@ const ContactForm = () => {
         </label>
         <input
           id="email"
-          name="email"
           className={style.input}
           placeholder="Ваша електронна пошта*"
           data-empty-notice="Ми не зможемо з вами зв'язатись без електронної пошти"
-          {...register('email', { required: true })}
+          {...register('email', {
+            required: "Електронна пошта є обов'язковою",
+          })}
         />
         {errors.email && (
           <span className={style.error}>
-            {errors.email.message || errors.email.ref.dataset.emptyNotice}
+            {errors.email.message || errors.email.ref?.dataset?.emptyNotice}
           </span>
         )}
       </div>
@@ -79,14 +85,13 @@ const ContactForm = () => {
         </label>
         <textarea
           id="message"
-          name="message"
           className={style.input}
           data-empty-notice="Залиште нам хоч якесь повідомлення"
-          {...register('message', { required: true })}
+          {...register('message', { required: "Повідомлення є обов'язковим" })}
         ></textarea>
         {errors.message && (
           <span className={style.error}>
-            {errors.message.message || errors.message.ref.dataset.emptyNotice}
+            {errors.message.message || errors.message.ref?.dataset?.emptyNotice}
           </span>
         )}
       </div>
