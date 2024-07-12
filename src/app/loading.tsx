49f37146b711b0821b1a1a5cd56823dba71e762f
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-07-12 14:39:57 +0300

    added error.tsx loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <html>
+      <body>
+        <div>
+          <p>Loading ...</p>
+        </div>
+      </body>
+    </html>
+  );
+}
