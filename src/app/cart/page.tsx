42eca46c04ab51090fa98552609959c2adf8e1ac
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-11-14 18:10:44 +0200

    add useSelector to cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,3 +1,27 @@
+'use client';
+
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/app/redux/store';
+
 export default function Cart() {
-  return <>Cart</>;
+  const cartItems = useSelector((state: RootState) => state.cart.items);
+  console.log('cartItems', cartItems);
+
+  return (
+    <div>
+      <h2>Ваш кошик</h2>
+      {cartItems.length > 0 ? (
+        cartItems.map((item) => (
+          <div key={item.id}>
+            <p>Назва продукту: {item.article}</p>
+            <p>Кількість: {item.quantity}</p>
+            <p>Загальна сума: {item.total} ₴</p>
+          </div>
+        ))
+      ) : (
+        <p>Кошик порожній</p>
+      )}
+    </div>
+  );
 }
--- src/app/redux/Provider.ts
+++ src/app/redux/Provider.ts
@@ -3,10 +3,3 @@
 import { Provider } from 'react-redux';
 
 export default Provider;
-
-// import { Carousel } from 'acme-carousel';
-
-// export default Carousel;
-
-// =====================
-// import Carousel from './carousel'
