485080acb41308470b7a84344af8e6e22b31eaf1
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-11-14 12:16:51 +0200

    added store

---
+++ src/app/redux/Provider.ts
@@ -0,0 +1,12 @@
+'use client';
+
+import { Provider } from 'react-redux';
+
+export default Provider;
+
+// import { Carousel } from 'acme-carousel';
+
+// export default Carousel;
+
+// =====================
+// import Carousel from './carousel'
--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -2,42 +2,42 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import cartSlice from './cartSlice';
-// import {
-//   createAction,
-//   createReducer,
-//   UnknownAction,
-//   PayloadAction,
-// } from '@reduxjs/toolkit';
+import {
+  createAction,
+  createReducer,
+  UnknownAction,
+  PayloadAction,
+} from '@reduxjs/toolkit';
 
-// interface CounterState {
-//   value: number;
-//   article: string;
-// }
-// const initialState: CounterState = { value: 0, article: '' };
+interface CounterState {
+  value: number;
+  article: string;
+}
+const initialState: CounterState = { value: 0, article: '' };
 
-// interface ArticlePayload {
-//   value: number;
-//   article: string;
-// }
+interface ArticlePayload {
+  value: number;
+  article: string;
+}
 
-// const articleIncrement = createAction<ArticlePayload>('myValue/increment');
-// const articleDecrement = createAction<ArticlePayload>('myValue/decrement');
+const articleIncrement = createAction<ArticlePayload>('myValue/increment');
+const articleDecrement = createAction<ArticlePayload>('myValue/decrement');
 
-// const orderReducer = createReducer(
-//   initialState,
-//   (builder) => {
-//     builder
-//       .addCase(articleIncrement, (state, action) => {
-//         state.value += action.payload.value;
-//         state.article = action.payload.article;
-//       })
-//       .addCase(articleDecrement, (state, action) => {
-//         state.value -= action.payload.value;
-//         state.article = action.payload.article;
-//       });
+const orderReducer = createReducer(
+  initialState,
+  (builder) => {
+    builder
+      .addCase(articleIncrement, (state, action) => {
+        state.value += action.payload.value;
+        state.article = action.payload.article;
+      })
+      .addCase(articleDecrement, (state, action) => {
+        state.value -= action.payload.value;
+        state.article = action.payload.article;
+      });
       
-//   }
-// );
+  }
+);
 
 export const store = configureStore({
   reducer: {
